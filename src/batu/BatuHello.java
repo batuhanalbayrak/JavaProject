edc7c127bf1d23a172d199b4100dc2b9fbe8b892
batuhanalbayrak <bthnalbayrak@gmail.com>
2022-12-26 17:22:29 +0300

    batu branchi güncellendi

---
+++ src/batu/BatuHello.java
@@ -0,0 +1,8 @@
+package batu;
+
+public class BatuHello {
+    public static void main(String[] args) {
+
+        System.out.println("Ben şimdi yeni bir branch oluşturdum ve oradayım. Batu branchigit add .");
+    }
+}
