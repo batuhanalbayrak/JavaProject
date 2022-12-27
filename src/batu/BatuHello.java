3279129e296fabc64607a935e186ca41f54d5ea0
batuhanalbayrak <bthnalbayrak@gmail.com>
2022-12-27 15:35:34 +0300

    batuda değişiklik yaptım

--- src/batu/BatuHello.java
+++ src/batu/BatuHello.java
@@ -4,5 +4,6 @@ public class BatuHello {
     public static void main(String[] args) {
 
         System.out.println("Ben şimdi yeni bir branch oluşturdum ve oradayım. Batu branchigit add .");
+        System.out.println("batu branchınde değişiklik yaptım");
     }
 }
